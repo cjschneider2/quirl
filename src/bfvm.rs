b80e60664597cbd86cd81be0126ddea0199e2575
cjschneider2 <cjschneider2@gmail.com>
2015-07-06 16:29:58 +0200

    NON-WORKING: Changed the datatype in prog_state from a String to a Vec for usability reasons.

--- src/bfvm.rs
+++ src/bfvm.rs
@@ -9,7 +9,9 @@ pub struct VM {
     jmps : HashMap<i16, i16>,
 }
 impl VM {
-    fn tokenize (input: &str) -> Option<(String,HashMap<i16,i16>)> {
+    /// parses the imput string, only keeping valid characters. Also generates the
+    /// jump table used for the '[' and ']' characters.
+    fn parse(input: &str) -> Option<(String,HashMap<i16,i16>)> {
         let mut out_str = String::new();
         let mut jump_tbl: HashMap<i16,i16> = HashMap::new();
         let mut jmps = Vec::new(); // used to store the intermediate locations
@@ -37,14 +39,49 @@ impl VM {
         }
         Some((out_str,jump_tbl))
     }
+    /// runs the VM with the specified input string. Returns and output object containing
+    /// the final runtime status of the program as well as any collected output.
     pub fn run_with_input (input: &str) -> ProgOutput {
-        let (prg_str, jmp_tbl) = VM::tokenize(input).unwrap();
+        let (prg_str, jmp_tbl) = VM::parse(input).unwrap();
         let mut bf_prog = VM {
             vms: VMState::new(),
             prg: ProgState::new( prg_str.to_string() ),
             jmps: jmp_tbl,
         };
-        let output = bf_prog.prg.run(&mut bf_prog.vms);
+        let output = bf_prog.run();
+        output
+    }
+    fn run (&mut self) -> ProgOutput {
+        let mut std_out = "".to_string();
+        let mut output = ProgOutput {
+            status: None,
+            std_out: std_out,
+        };
+        let mut running = true;
+        while(running){
+            // Execute the current instruction
+            match self.prg.get_next() {
+                '+' => (),
+                '-' => (),
+                '>' => (),
+                '<' => (),
+                '.' => (),
+                ',' => (),
+                '[' => {
+                        ();
+                       },
+                ']' => {
+                        ();
+                       },
+                _ => (),
+            }
+            // Inc. program pointer if we have more program to execute
+            if self.prg.get_pp() < self.prg.get_len() {
+                self.prg.inc_pp();
+            } else {
+                running = false;
+            }
+        }
         output
     }
 }
@@ -53,12 +90,22 @@ impl VM {
 mod test {
     use super::VM;
     #[test]
-    fn tok_valid_symbol_test () {
+    fn test_parse_invalid_symbols() {
         let in_str = r#"[]++++++++++[>>+>+>++++++[<<+<+++>>>-]<<<<-]
             "A*$";?@![#>>+<<]>[>>]<<<<[>++<[-]]>.>."#;
         let out_str = r#"[]++++++++++[>>+>+>++++++[<<+<+++>>>-]<<<<-][>>+<<]>[>>]<<<<[>++<[-]]>.>."#;
         let o_res = out_str.to_string();
-        let (res,jmp_tbl) = VM::tokenize(in_str).unwrap();
+        let (res,jmp_tbl) = VM::parse(in_str).unwrap();
         assert_eq!(res, o_res);
+    }
+    #[test]
+    fn test_run_with_input() {
+        let in_str = r#"[]++++++++++[>>+>+>++++++[<<+<+++>>>-]<<<<-]
+            "A*$";?@![#>>+<<]>[>>]<<<<[>++<[-]]>.>."#;
+        let out_str = r#"[]++++++++++[>>+>+>++++++[<<+<+++>>>-]<<<<-][>>+<<]>[>>]<<<<[>++<[-]]>.>."#;
+        let o_res = out_str.to_string();
+        let output = VM::run_with_input(in_str);
+        println!("{:?}", output);
+        assert!(false);
     }
 }
--- src/prog_state.rs
+++ src/prog_state.rs
@@ -1,27 +1,44 @@
 use vm_state::VMState;
 
 /// Stores the string representation of the current program as a string slice.
+#[derive(Debug)]
 pub struct ProgOutput {
-    status : Option<String>,
-    std_out: String,
+    pub status : Option<String>,
+    pub std_out: Vec<u8>,
 }
 pub struct ProgState {
-    program : String,
+    program : Vec<u8>,
     pp : usize,
+    len: usize,
 }
 impl ProgState {
     pub fn new (input: String) -> ProgState {
-        ProgState {
+        let mut ret_val = ProgState {
             program : input,
             pp : 0,
-        }
-    }
-    pub fn run (&self, vm: &mut VMState) -> ProgOutput {
-        let mut std_out = "".to_string();
-        let mut output = ProgOutput {
-            status: None,
-            std_out: std_out,
+            len: 0,
         };
-        output
+        ret_val.set_len();
+        ret_val
+    }
+    fn set_len(&mut self) {
+        self.len = self.program.len();
+    }
+
+    fn inc_pp(&mut self) {
+        self.pp += 1;
+    }
+
+    pub fn get_pp(&self) -> usize {
+        self.pp
+    }
+
+    pub fn get_len(&self) -> usize {
+        self.len
+    }
+
+    pub fn get_next_char(&self) -> Option<&u8> {
+        self.inc_pp;
+        self.program.get(self.pp)
     }
 }
