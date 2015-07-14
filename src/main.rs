a139b95999346a4c3871fbbd193fb02e8ad9aa34
cjschneider2 <cjschneider2@gmail.com>
2015-07-14 10:20:10 +0200

    Added a REPL to the main program

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,53 @@ mod vm_state;
 mod prog_state;
 mod bfvm;
 
+use std::io;
+use std::io::{Write};
+
 #[cfg(not(test))]
 fn main() {
-    let prog_str = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-    let res = bfvm::VM::run_with_input(prog_str);
-    // Convert the output list to a UTF8 (ANSCII) String slice
+    // The welcome text in beautiful bf bytecode :P
+    let welcome_prog = "+[--->++<]>+.++[->++++<]>+.+++++++.----
+        -----.++++++++++++.--.--------.--[--->+<]>-.---[->++++<
+        ]>.-----.[--->+<]>-----.---[->++++<]>.------------.---.
+        --[--->+<]>-.[-->+++++<]>+.---[-->+++<]>.------------.+
+        ++++++++.------.[++>---<]>--.+[->++<]>.---[----->+<]>-.
+        +++[->+++<]>++.++++++++.+++++.--------.-[--->+<]>--.+[-
+        >+++<]>+.++++++++.-[++>---<]>+.++++[->++<]>+.+[--->+<]>
+        .++++++.+++[->+++<]>.+++++++++++++.--.++.-------------.
+        [--->+<]>---.+++[->+++<]>.+++++++++++++.[--->+<]>-----.";
+    let res = bfvm::VM::run_with_input(welcome_prog);
     let out_str = std::str::from_utf8(&res.std_out);
     match out_str {
         Ok(string) => println!("{}", string),
         Err(error) => println!("{}", error),
+    }
+    println!("");
+    // The actual REPL starts here
+    loop {
+        // Read
+        print!("> ");
+        let _  = io::stdout().flush();
+        let mut input = String::new();
+        let in_len = io::stdin()
+                        .read_line(&mut input)
+                        .ok()
+                        // This is a little bit of a cop-out until I
+                        // figure out what to do with invalid input
+                        .unwrap_or(0);
+        if in_len > 0 {
+            // Evaluate
+            let result = bfvm::VM::run_with_input(&input);
+            // Print
+            let out_str = std::str::from_utf8(&result.std_out);
+            match out_str {
+                Ok(string) => println!("{}", string),
+                Err(error) => println!("{}", error),
+            }
+        } else {
+            println!("Unhelpful rrror message about bad input.")
+        }
+        // Loop
     }
 }
 
@@ -20,6 +58,7 @@ mod test {
     use bfvm;
     use prog_state::ProgOutput;
     use std;
+
     #[test]
     fn output_2_program() {
         let prog_string = "++.";
@@ -40,30 +79,28 @@ mod test {
     }
 
 
-    // The following tests are obtained from :
-    //    Daniel B Cristofani (cristofdathevanetdotcom)
-    //    http://www.hevanet.com/cristofd/brainfuck/
-    //    Here are some little programs for testing brainfuck implementations.
+    /// The following tests are obtained from :
+    ///    Daniel B Cristofani (cristofdathevanetdotcom)
+    ///    http://www.hevanet.com/cristofd/brainfuck/
+    ///    Here are some little programs for testing brainfuck implementations.
 
 
-    /*
-    >,>+++++++++,>+++++++++++[<++++++<++++++<+>>>-]<<.>.<<-.>.>.<<.
-    This is for testing i/o; give it a return followed by an EOF. (Try it both
-    with file input--a file consisting only of one blank line--and with
-    keyboard input, i.e. hit return and then ctrl-d (Unix) or ctrl-z
-    (Windows).)
-    It should give two lines of output; the two lines should be identical, and
-    should be lined up one over the other. If that doesn't happen, ten is not
-    coming through as newline on output.
-    The content of the lines tells how input is being processed; each line
-    should be two uppercase letters.
-    Anything with O in it means newline is not coming through as ten on input.
-    LK means newline input is working fine, and EOF leaves the cell unchanged
-    (which I recommend).
-    LB means newline input is working fine, and EOF translates as 0.
-    LA means newline input is working fine, and EOF translates as -1.
-    Anything else is fairly unexpected.
-    */
+    /// >,>+++++++++,>+++++++++++[<++++++<++++++<+>>>-]<<.>.<<-.>.>.<<.
+    /// This is for testing i/o; give it a return followed by an EOF. (Try it both
+    /// with file input--a file consisting only of one blank line--and with
+    /// keyboard input, i.e. hit return and then ctrl-d (Unix) or ctrl-z
+    /// (Windows).)
+    /// It should give two lines of output; the two lines should be identical, and
+    /// should be lined up one over the other. If that doesn't happen, ten is not
+    /// coming through as newline on output.
+    /// The content of the lines tells how input is being processed; each line
+    /// should be two uppercase letters.
+    /// Anything with O in it means newline is not coming through as ten on input.
+    /// LK means newline input is working fine, and EOF leaves the cell unchanged
+    /// (which I recommend).
+    /// LB means newline input is working fine, and EOF translates as 0.
+    /// LA means newline input is working fine, and EOF translates as -1.
+    /// Anything else is fairly unexpected.
     #[test]
     fn test_new_line() {
         let prog_str = ">,>+++++++++,>+++++++++++[<++++++<++++++<+>>>-]<<.>.<<-.>.>.<<.";
@@ -76,12 +113,10 @@ mod test {
         assert_eq!(res, "BB\nBB\n");
     }
 
-    /*
-    ++++[>++++++<-]>[>+++++>+++++++<<-]>>++++<[[>[[>>+<<-]<]>>>-]>-[>+>+<<-]>]
-    +++++[>+++++++<<++>-]>.<<.
-    Goes to cell 30000 and reports from there with a #. (Verifies that the
-    array is big enough.)
-    */
+    /// ++++[>++++++<-]>[>+++++>+++++++<<-]>>++++<[[>[[>>+<<-]<]>>>-]>-[>+>+<<-]>]
+    /// +++++[>+++++++<<++>-]>.<<.
+    /// Goes to cell 30000 and reports from there with a #. (Verifies that the
+    /// array is big enough.)
     #[test]
     fn test_cell_30000() {
         let prog_str = "++++[>++++++<-]>[>+++++>+++++++<<-]>>++++<[[>[[>>+<<-]<]>>>-]>-[>+>+<<-]>]+++++[>+++++++<<++>-]>.<<.";
@@ -112,11 +147,9 @@ mod test {
     +[>+++++++++++++++++++++++++++++++++.]
     */
 
-    /*
-    []++++++++++[>>+>+>++++++[<<+<+++>>>-]<<<<-]
-    "A*$";?@![#>>+<<]>[>>]<<<<[>++<[-]]>.>.
-    Tests for several obscure problems. Should output an H.
-    */
+    /// []++++++++++[>>+>+>++++++[<<+<+++>>>-]<<<<-]
+    /// "A*$";?@![#>>+<<]>[>>]<<<<[>++<[-]]>.>.
+    /// Tests for several obscure problems. Should output an H.
     #[test]
     fn test_obscure_problems() {
         let prog_str = r#""[]++++++++++[>>+>+>++++++[<<+<+++>>>-]<<<<-]"A*$";?@![#>>+<<]>[>>]<<<<[>++<[-]]>.>."#;
@@ -129,15 +162,12 @@ mod test {
         assert_eq!(res, "H\n");
     }
 
-    /*
-    +++++[>+++++++>++<<-]>.>.[
-    Should ideally give error message "unmatched [" or the like, and not give
-    any output. Not essential.
-
-    +++++[>+++++++>++<<-]>.>.][
-    Should ideally give error message "unmatched ]" or the like, and not give
-    any output. Not essential.
-    */
+    /// +++++[>+++++++>++<<-]>.>.[
+    /// Should ideally give error message "unmatched [" or the like, and not give
+    /// any output. Not essential.
+    /// +++++[>+++++++>++<<-]>.>.][
+    /// Should ideally give error message "unmatched ]" or the like, and not give
+    /// any output. Not essential.
     #[test]
     fn test_unmatched_l_bracket() {
         let prog_str = r#"+++++[>+++++++>++<<-]>.>.["#;
